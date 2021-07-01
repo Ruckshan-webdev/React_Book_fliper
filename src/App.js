04d91d271f1b6278d5c3b530ee4f0620818bbd57
Ruckshan-webdev <81849722+Ruckshan-webdev@users.noreply.github.com>
2021-06-30 21:23:17 -0700

    Initial Upload

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import HTMLFlipBook from "react-pageflip";
+import './app.css'
+import FrontCover from './FronPage';
+import BackCover from './BackPage';
+import SinglePage from './SinglePage'
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 0,
+      totalPage: 0,
+    };
+  }
+
+  nextButtonClick = () => {
+    this.flipBook.getPageFlip().flipNext();
+  };
+
+  prevButtonClick = () => {
+    this.flipBook.getPageFlip().flipPrev();
+  };
+
+  onPage = (e) => {
+    this.setState({
+      page: e.data,
+    });
+  };
+
+  componentDidMount() {
+    this.setState({
+      totalPage: this.flipBook.getPageFlip().getPageCount(),
+    });
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <HTMLFlipBook
+          width={320}
+          height={420}
+          size="fixed"
+          minWidth={315}
+          maxWidth={1000}
+          minHeight={400}
+          maxHeight={1533}
+          maxShadowOpacity={0.5}
+          showCover={true}
+          mobileScrollSupport={true}
+          onFlip={this.onPage}
+          onChangeOrientation={this.onChangeOrientation}
+          onChangeState={this.onChangeState}
+          className="book"
+          ref={(el) => (this.flipBook = el)}
+        >
+
+          <FrontCover></FrontCover>
+
+          {/* Page 01 */}
+          <SinglePage number={1} heading="Myheading" image="img.jpg" button="Click Me">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore.</SinglePage>
+
+          {/* Page 02 */}
+          <SinglePage number={2} image="img.jpg" button="Page Link">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</SinglePage>
+
+          {/* Page 03 */}
+          <SinglePage number={3}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</SinglePage>
+
+          {/* Page 04 */}
+
+
+
+
+
+
+
+
+
+          
+          <SinglePage number={4}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</SinglePage>
+
+          {/* Page 05 */}
+          <SinglePage number={5}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</SinglePage>
+
+          {/* Page 06 */}
+          <SinglePage number={6}>At vero eos et accusamus et iusto dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</SinglePage>
+         
+          {/* Page 07 */}
+          <SinglePage number={7}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</SinglePage>
+          
+          {/* Page 08 */}
+          <SinglePage number={8}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</SinglePage>
+
+          <BackCover ></BackCover>
+
+        </HTMLFlipBook>
+
+        {/* BUTTONS */}
+        <div className="container">
+            <button type="button" onClick={this.prevButtonClick} className="btn">
+              Previous page
+            </button>
+            <p>
+              <span>{this.state.page}</span> &nbsp;  of &nbsp;
+              <span>{this.state.totalPage}</span>
+            </p>
+            <button type="button" onClick={this.nextButtonClick} className="btn">
+                Next page
+            </button>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
